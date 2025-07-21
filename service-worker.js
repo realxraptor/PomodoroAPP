1dd13f086a6b548dd38d4b61cf4139fe7d9ba927
realxraptor <ahmetbsarpkaya@gmail.com>
2025-07-21 17:18:51 +0300

    PWA

---
+++ service-worker.js
@@ -0,0 +1,41 @@
+// serviceWorker.js
+const CACHE_NAME = "v1.0.5";
+const CACHE_ASSETS = [
+  "index.html",
+  "images/icon1.jpg",
+  "manifest.json"
+];
+
+// Kurulum (Install)
+self.addEventListener("install", () => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(CACHE_ASSETS);
+    })
+  );
+});
+
+// Aktifleştirme (Activate)
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Fetch
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
+
